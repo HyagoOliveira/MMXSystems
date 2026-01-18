7ed70d8a0cdd8b231ae4cfb4fd31f7e75b6b9bbf
Hyago Oliveira <hyagogow@gmail.com>
2026-01-18 12:09:09 -0300

    Add ElementType

---
+++ Scripts/_CoreSystem/ElementType.cs
@@ -0,0 +1,17 @@
+namespace MMX.CoreSystem
+{
+    /// <summary>
+    /// All elements in the game.
+    /// </summary>
+    public enum ElementType
+    {
+        None,
+        Plasma,
+        Fire,
+        Ice,
+        Water,
+        Wind,
+        Light,
+        Electricity
+    }
+}
