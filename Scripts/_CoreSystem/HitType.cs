a947d001b13cd86be7e520e82cd799e5cf61f31c
Hyago Oliveira <hyagogow@gmail.com>
2026-01-26 21:35:09 -0300

    Move HitType

---
+++ Scripts/_CoreSystem/HitType.cs
@@ -0,0 +1,18 @@
+namespace MMX.CoreSystem
+{
+    /// <summary>
+    /// All Hit types in the game.
+    /// </summary>
+    public enum HitType
+    {
+        Bullet_00 = 0,
+        Bullet_01,
+        Bullet_02,
+
+        Slash_00 = 10,
+
+        Spark_00 = 20,
+
+        Invulnerable = 30
+    }
+}
